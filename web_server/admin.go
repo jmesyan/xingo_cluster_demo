77087c6146d06ba6d258c6dc5740e0d4f27cf070
yanjq <yanjq@realbull.net>
2018-01-07 23:55:25 +0800

    session测试

--- web_server/admin.go
+++ web_server/admin.go
@@ -3,18 +3,54 @@ package web_server
 import (
 	"fmt"
 	"github.com/celrenheit/lion"
+	"github.com/goincremental/negroni-sessions"
 	"net/http"
 )
 
 var (
 	Monitor *lion.Router
+	Api     *lion.Router
 )
 
 func init() {
 	Monitor = lion.New()
 	Monitor.GetFunc("/hello", Hello)
+	Monitor.GetFunc("/setSession", setSession)
+	Monitor.GetFunc("/showSession", showSession)
+	Monitor.GetFunc("/clearSession", clearSession)
+
+	Api = lion.New()
+	Api.GetFunc("/showSession", showSession)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "this is the admin hello method")
+}
+
+func setSession(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
+	session.Set("hello", "world")
+	fmt.Fprintf(w, "setOK")
+}
+
+func clearSession(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
+	session.Clear()
+	fmt.Fprintf(w, "clearOK")
+}
+
+func showSession(w http.ResponseWriter, r *http.Request) {
+	session := sessions.GetSession(r)
+	if session == nil {
+		fmt.Fprintf(w, "session is not set")
+	} else {
+		hello := session.Get("hello")
+		if hello == nil {
+			fmt.Fprintf(w, "hello is not set")
+		} else {
+			fmt.Fprintf(w, hello.(string))
+		}
+
+	}
+
 }
--- web_server/web_api.go
+++ web_server/web_api.go
@@ -1,16 +1,38 @@
 package web_server
 
 import (
+	"fmt"
 	"github.com/celrenheit/lion"
+	"github.com/goincremental/negroni-sessions"
+	"github.com/goincremental/negroni-sessions/cookiestore"
 	"github.com/jmesyan/xingo/logger"
-	// "net/http"
+	"github.com/urfave/negroni"
+	"net/http"
 )
 
 type WebApi struct {
 }
 
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is the custum not found")
+}
+
+func NegroniWrap(h negroni.HandlerFunc) func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		nw := negroni.NewResponseWriter(w)
+		h(nw, r, next)
+	}
+}
+
 func (this *WebApi) Ready(r *lion.Router) {
+	nh := http.HandlerFunc(NotFound)
+	lion.WithNotFoundHandler(nh)(r)
+	store := cookiestore.New([]byte("secret123"))
+	session := sessions.Sessions("my_session", store)
 	admin := r.Group("/admin")
+	admin.UseNext(NegroniWrap(session))
 	admin.Mount("/monitor", Monitor)
+	api := r.Group("/api")
+	api.Mount("/sess", Api)
 	logger.Info("the web api ready")
 }
